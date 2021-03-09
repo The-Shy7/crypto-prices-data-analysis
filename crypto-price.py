845362de90f832774d21d031042ac0e56e3f57cf
The-Shy7 <tahmintalukder98@gmail.com>
2021-03-08 19:34:32 -0800

    imported libraries

---
+++ crypto-price.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title('Crypto Prices')
+st.markdown(""" 
+    This app retrieves cryptocurrency prices for the top 100 cryptocurrency from **CoinMarketCap**
+""")
