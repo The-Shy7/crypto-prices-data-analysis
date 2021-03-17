e6d8ff44c287071c82d1d1a5516a90196e2fa761
The-Shy7 <tahmintalukder98@gmail.com>
2021-03-16 19:14:31 -0700

    added expander for about info

--- crypto-price.py
+++ crypto-price.py
@@ -2,7 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# title
 st.title('Crypto Prices')
 st.markdown(""" 
-    This app retrieves cryptocurrency prices for the top 100 cryptocurrency from **CoinMarketCap**
+    This app retrieves cryptocurrency prices for the top 100 cryptocurrencies from **CoinMarketCap**
+""")
+
+# about 
+expander_bar = st.beta_expander("About")
+expander_bar.markdown("""
+* **Python libraries:** 
+* **Data source:** [CoinMarketCap](http://coinmarketcap.com)
 """)
