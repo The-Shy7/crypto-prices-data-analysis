8193df6f8481df85ec4f01e002da8d552208ba74
The-Shy7 <tahmintalukder98@gmail.com>
2021-03-16 19:27:23 -0700

    working on sidebar column

--- crypto-price.py
+++ crypto-price.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# page layout
+# page expands to full width
+st.set_page_config(layout="wide")
+
 # title
 st.title('Crypto Prices')
 st.markdown(""" 
@@ -14,3 +18,14 @@ expander_bar.markdown("""
 * **Python libraries:** 
 * **Data source:** [CoinMarketCap](http://coinmarketcap.com)
 """)
+
+# divide page to 3 columns
+# col1 = sidebar, col2/col3 = page content
+col1 = st.sidebar
+col2, col3 = st.beta_columns((2,1))
+
+# sidebar + main panel
+col1.header('Input Options')
+
+# currency selector
+currency_price_unit = col1.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
